b05ecc4a506589f3aa82e1493d4d2dbbd50bfc65
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-05-02 13:42:45 +0900

    정근브렌치로 체크아웃

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("정근브렌치 여기임");
+        System.out.println("1번주자");
 
     }
 }
