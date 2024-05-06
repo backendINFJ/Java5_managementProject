87fe17ba1a726d6a3ae71dfb6528eb173ffa6082
leejinuk <leejinouk123@kakao.com>
2024-05-07 00:24:12 +0900

    update: Student.java에 socre를 기록하는 hashMap 필드가 추가됐음.

--- src/Student.java
+++ src/Student.java
@@ -1,22 +1,24 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Student {
     private String studentId;
-
     private String studentName;
     private ArrayList<String> studentSubjectList = new ArrayList<>();
+    private Map<String, int[]> studentScoreMap = new HashMap<>();
 
     // Student 기본생성자
+
     public Student(String studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
     }
+    // Getter
 
-    // GEtter
     public String getStudentId() {
         return studentId;
     }
-
     public String getStudentName() {
         return studentName;
     }
@@ -24,10 +26,20 @@ public class Student {
     public ArrayList<String> getStudentSubjectList() {
         return studentSubjectList;
     }
-    //Setter
 
+    //Setter
     public void setStudentSubjectList(ArrayList<String> studentSubjectList) {
         this.studentSubjectList = studentSubjectList;
+    }
+
+    //점수 setter
+    public void setStudentScoreMap(Map<String, int[]> studentScoreMap) {
+        this.studentScoreMap = studentScoreMap;
+    }
+
+    //점수 getter
+    public Map<String, int[]> getStudentScoreMap() {
+        return studentScoreMap;
     }
 }
 
