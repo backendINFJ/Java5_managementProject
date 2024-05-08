41653a0942912b1c2c361755f1ebde4159a6ed08
JPARK <skfkrn2075@gmail.com>
2024-05-08 11:59:13 +0900

    학생 상태 이름 get set 메소드 추가

--- src/Student.java
+++ src/Student.java
@@ -5,22 +5,29 @@ import java.util.Map;
 public class Student {
     private String studentId;
     private String studentName;
+    private String studentStatus;
     private ArrayList<String> studentSubjectList = new ArrayList<>();
     private Map<String, int[]> studentScoreMap = new HashMap<>();
 
     // Student 기본생성자
 
-    public Student(String studentId, String studentName) {
+    public Student(String studentId, String studentName, String studentStatus) {
         this.studentId = studentId;
         this.studentName = studentName;
+        this.studentStatus = studentStatus;
     }
-    // Getter
 
+    // Getter
     public String getStudentId() {
         return studentId;
     }
     public String getStudentName() {
         return studentName;
+    }
+
+    // 이름 Setter
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
     }
 
     public ArrayList<String> getStudentSubjectList() {
@@ -32,6 +39,7 @@ public class Student {
         this.studentSubjectList = studentSubjectList;
     }
 
+
     //점수 setter
     public void setStudentScoreMap(Map<String, int[]> studentScoreMap) {
         this.studentScoreMap = studentScoreMap;
@@ -40,6 +48,15 @@ public class Student {
     //점수 getter
     public Map<String, int[]> getStudentScoreMap() {
         return studentScoreMap;
+    }
+
+    //상태 setter
+    public void setStudentStatus(String studentStatus) {
+        this.studentStatus = studentStatus;
+    }
+    //상태 getter
+    public String getStudentStatus() {
+        return studentStatus;
     }
 }
 
